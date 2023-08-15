6629748536aeb90d49a74f72f64db8aa6224e61b
Hoàng Văn Tùng <tungvanzk03@gmail.com>
2023-08-15 16:06:02 +0700

    update js

--- drumkit.js
+++ drumkit.js
@@ -1,14 +1,98 @@
 const drumTotal = document.querySelectorAll(".container > div");
 
-const drumCount = drumTotal.length;
-for (let i = 0; i < drumCount; i++) {
-    drumTotal[i].addEventListener("click", () => {
-        makeSounds(drumTotal[i].dataset.keypress);
-    });
-}
+drumTotal[0].addEventListener("click", () => {
+    var hiHatOpen = new Audio("./sounds/hi-hat-open.mp3");
+    hiHatOpen.play();
+});
+drumTotal[1].addEventListener("click", () => {
+    var hiHatClosed = new Audio("./sounds/hi-hat-closed.mp3");
+    hiHatClosed.play();
+});
+drumTotal[2].addEventListener("click", () => {
+    var hiHatFoot = new Audio("./sounds/hi-hat-foot.mp3");
+    hiHatFoot.play();
+});
+drumTotal[3].addEventListener("click", () => {
+    var crashCymbal = new Audio("./sounds/crash-cymbal.mp3");
+    crashCymbal.play();
+});
+drumTotal[4].addEventListener("click", () => {
+    var snareDrum = new Audio("./sounds/snare-drum.mp3");
+    snareDrum.play();
+});
+drumTotal[5].addEventListener("click", () => {
+    var snareDrumCs = new Audio("./sounds/snare-drum-cs.mp3");
+    snareDrumCs.play();
+});
+drumTotal[6].addEventListener("click", () => {
+    var highTom = new Audio("./sounds/high-tom.mp3");
+    highTom.play();
+});
+drumTotal[7].addEventListener("click", () => {
+    var lowTom = new Audio("./sounds/low-tom.mp3");
+    lowTom.play();
+});
+drumTotal[8].addEventListener("click", () => {
+    var bassDrum = new Audio("./sounds/bass-drum.mp3");
+    bassDrum.play();
+});
+drumTotal[9].addEventListener("click", () => {
+    var floorTom = new Audio("./sounds/floor-tom.mp3");
+    floorTom.play();
+});
+drumTotal[10].addEventListener("click", () => {
+    var rideCymbal = new Audio("./sounds/ride-cymbal.mp3");
+    rideCymbal.play();
+});
+
 
 document.addEventListener("keypress", (event) => {
-    makeSounds(event.key);
+    switch (event.key) {
+        case "e": case "w":
+            var hiHatOpen = new Audio("./sounds/hi-hat-open.mp3");
+            hiHatOpen.play();
+            break;
+        case "r": case "t":
+            var hiHatClosed = new Audio("./sounds/hi-hat-closed.mp3");
+            hiHatClosed.play();
+            break;
+        case "c":
+            var hiHatFoot = new Audio("./sounds/hi-hat-foot.mp3");
+            hiHatFoot.play();
+            break;
+        case "y":
+            var crashCymbal = new Audio("./sounds/crash-cymbal.mp3");
+            crashCymbal.play();
+            break;
+        case "s": case "a":
+            var snareDrum = new Audio("./sounds/snare-drum.mp3");
+            snareDrum.play();
+            break;
+        case "d": case "f":
+            var snareDrumCs = new Audio("./sounds/snare-drum-cs.mp3");
+            snareDrumCs.play();
+            break;
+        case "g":
+            var highTom = new Audio("./sounds/high-tom.mp3");
+            highTom.play();
+            break;
+        case "h":
+            var lowTom = new Audio("./sounds/low-tom.mp3");
+            lowTom.play();
+            break;
+        case "x": case "z":
+            var bassDrum = new Audio("./sounds/bass-drum.mp3");
+            bassDrum.play();
+            break;
+        case "j":
+            var floorTom = new Audio("./sounds/floor-tom.mp3");
+            floorTom.play();
+            break;
+        case "u":
+            var rideCymbal = new Audio("./sounds/ride-cymbal.mp3");
+            rideCymbal.play();
+            break;
+    }
 });
 
 function makeSounds(keypress) {
