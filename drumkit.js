5f342b0894888a11d7f2418eb3af48391da833e2
Hoàng Văn Tùng <tungvanzk03@gmail.com>
2023-08-15 16:56:51 +0700

    update

--- drumkit.js
+++ drumkit.js
@@ -60,100 +60,51 @@ drumTotal[10].addEventListener("click", () => {
 document.addEventListener("keypress", (event) => {
     switch (event.key) {
         case "e": case "w":
-            var hiHatOpen = new Audio("./sounds/hi-hat-open.mp3");
+            hiHatOpen.src = "./sounds/hi-hat-open.mp3";
             hiHatOpen.play();
             break;
         case "r": case "t":
-            var hiHatClosed = new Audio("./sounds/hi-hat-closed.mp3");
+            hiHatClosed.src = "./sounds/hi-hat-closed.mp3";
             hiHatClosed.play();
             break;
         case "c":
-            var hiHatFoot = new Audio("./sounds/hi-hat-foot.mp3");
+            hiHatFoot.src = "./sounds/hi-hat-foot.mp3";
             hiHatFoot.play();
             break;
         case "y":
-            var crashCymbal = new Audio("./sounds/crash-cymbal.mp3");
+            crashCymbal.src = "./sounds/crash-cymbal.mp3";
             crashCymbal.play();
             break;
         case "s": case "a":
-            var snareDrum = new Audio("./sounds/snare-drum.mp3");
+            snareDrum.src = "./sounds/snare-drum.mp3";
             snareDrum.play();
             break;
         case "d": case "f":
-            var snareDrumCs = new Audio("./sounds/snare-drum-cs.mp3");
+            snareDrumCs.src = "./sounds/snare-drum-cs.mp3";
             snareDrumCs.play();
             break;
         case "g":
-            var highTom = new Audio("./sounds/high-tom.mp3");
+            highTom.src = "./sounds/high-tom.mp3";
             highTom.play();
             break;
         case "h":
-            var lowTom = new Audio("./sounds/low-tom.mp3");
+            lowTom.src = "./sounds/low-tom.mp3";
             lowTom.play();
             break;
         case "x": case "z":
-            var bassDrum = new Audio("./sounds/bass-drum.mp3");
+            bassDrum.src = "./sounds/bass-drum.mp3";
             bassDrum.play();
             break;
         case "j":
-            var floorTom = new Audio("./sounds/floor-tom.mp3");
+            floorTom.src = "./sounds/floor-tom.mp3";
             floorTom.play();
             break;
         case "u":
-            var rideCymbal = new Audio("./sounds/ride-cymbal.mp3");
+            rideCymbal.src = "./sounds/ride-cymbal.mp3";
             rideCymbal.play();
             break;
     }
 });
-
-function makeSounds(keypress) {
-    switch (keypress) {
-        case "e": case "w":
-            var hiHatOpen = new Audio("./sounds/hi-hat-open.mp3");
-            hiHatOpen.play();
-            break;
-        case "r": case "t":
-            var hiHatClosed = new Audio("./sounds/hi-hat-closed.mp3");
-            hiHatClosed.play();
-            break;
-        case "c":
-            var hiHatFoot = new Audio("./sounds/hi-hat-foot.mp3");
-            hiHatFoot.play();
-            break;
-        case "y":
-            var crashCymbal = new Audio("./sounds/crash-cymbal.mp3");
-            crashCymbal.play();
-            break;
-        case "s": case "a":
-            var snareDrum = new Audio("./sounds/snare-drum.mp3");
-            snareDrum.play();
-            break;
-        case "d": case "f":
-            var snareDrumCs = new Audio("./sounds/snare-drum-cs.mp3");
-            snareDrumCs.play();
-            break;
-        case "g":
-            var highTom = new Audio("./sounds/high-tom.mp3");
-            highTom.play();
-            break;
-        case "h":
-            var lowTom = new Audio("./sounds/low-tom.mp3");
-            lowTom.play();
-            break;
-        case "x": case "z":
-            var bassDrum = new Audio("./sounds/bass-drum.mp3");
-            bassDrum.play();
-            break;
-        case "j":
-            var floorTom = new Audio("./sounds/floor-tom.mp3");
-            floorTom.play();
-            break;
-        case "u":
-            var rideCymbal = new Audio("./sounds/ride-cymbal.mp3");
-            rideCymbal.play();
-            break;
-    }
-}
 
 const showShortcutsElement = document.querySelector(".show-shortcuts");
 const shortcutsElements = document.querySelectorAll(".container span");
